c0b450451d2eda5c675c5c4ffa3dfd6cf70c4433
wesley1263 <weslei.andrade.souza@gmail.com>
2024-07-03 15:45:32 -0300

    Feat: Terraform gcp ajustado

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,28 +51,6 @@ resource "google_container_cluster" "primary" {
   }
 }
 
-resource "google_container_node_pool" "primary_nodes" {
-  name       = "primary-pool"
-  location   = google_container_cluster.primary.location
-  cluster    = google_container_cluster.primary.name
-  node_count = 2   # Reduzir o número de nós
-
-  node_config {
-    preemptible  = true
-    machine_type = "e2-medium"
-    disk_size_gb = 20   # Reduzir o tamanho do disco
-
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-      "https://www.googleapis.com/auth/servicecontrol",
-      "https://www.googleapis.com/auth/service.management.readonly",
-      "https://www.googleapis.com/auth/trace.append",
-    ]
-  }
-}
-
 output "kubernetes_cluster_name" {
   value = google_container_cluster.primary.name
 }
